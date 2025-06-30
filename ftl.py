663fac9b49e0bdde7cd19270390fe0ee7811dc79
nour ben romdhane <nour.ben-romdhane@dsv.com>
2025-06-30 11:26:22 +0200

    test 1

--- ftl.py
+++ ftl.py
@@ -1,2 +1,18 @@
 import pandas as pd
-import streamlit
+import streamlit as st
+
+
+st.title("Analyse FTL")
+
+# File uploader widget
+uploaded_file = st.file_uploader("Met le fichier ici ", type=["csv"])
+
+# If a file is uploaded
+if uploaded_file is not None:
+    try:
+        df = pd.read_csv(uploaded_file)
+        st.success("✅ File successfully uploaded and read!")
+        st.write("### Preview of your data:")
+        st.dataframe(df)
+    except Exception as e:
+        st.error(f"❌ An error occurred while reading the file: {e}")
